866b05807562d5ec84454627e4337239850edf71
Ramdas <ramdas.qualityfirst@gmail.com>
2026-04-08 11:40:25 +0530

    feat: add Prettier for code formatting and update test file for consistency

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,12 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://github.com/');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).fill('tester');
-  await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('test123');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+test("test", async ({ page }) => {
+  await page.goto("https://github.com/");
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await page
+    .getByRole("textbox", { name: "Username or email address" })
+    .click();
+  await page
+    .getByRole("textbox", { name: "Username or email address" })
+    .fill("tester");
+  await page
+    .getByRole("textbox", { name: "Username or email address" })
+    .press("Tab");
+  await page.getByRole("textbox", { name: "Password" }).fill("test123");
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+  await expect(page.getByRole("alert")).toContainText(
+    "Incorrect username or password.",
+  );
 });
