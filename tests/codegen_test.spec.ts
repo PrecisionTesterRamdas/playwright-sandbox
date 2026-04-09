fcc97b3ee5a8423e4bcb3f7cf362eb071e568be8
Ramdas <ramdas.qualityfirst@gmail.com>
2026-04-09 12:57:32 +0530

    fix: update login navigation in codegen_test.spec.ts for improved accuracy

--- tests/codegen_test.spec.ts
+++ tests/codegen_test.spec.ts
@@ -2,10 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://ecommerce-playground.lambdatest.io/");
-  await page.getByRole("link", { name: "Login" }).click();
-  await page.goto(
-    "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
-  );
+  await page.getByRole("button", { name: " My account" }).click();
   await page.getByRole("textbox", { name: "E-Mail Address" }).click();
   await page.getByRole("textbox", { name: "E-Mail Address" }).fill("Ramdas");
   await page.getByRole("textbox", { name: "Password" }).click();
