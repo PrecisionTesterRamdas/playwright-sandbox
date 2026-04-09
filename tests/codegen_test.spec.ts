ee5910679255c5c3b7306e8112d1340e092b7870
Ramdas <ramdas.qualityfirst@gmail.com>
2026-04-09 12:42:38 +0530

    fix: update playwright.config.ts for consistent formatting and add test cases created with the help of Codegen

---
+++ tests/codegen_test.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://ecommerce-playground.lambdatest.io/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.goto(
+    "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
+  );
+  await page.getByRole("textbox", { name: "E-Mail Address" }).click();
+  await page.getByRole("textbox", { name: "E-Mail Address" }).fill("Ramdas");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("Ram@123");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByText("Warning: No match for E-Mail")).toBeVisible();
+});
