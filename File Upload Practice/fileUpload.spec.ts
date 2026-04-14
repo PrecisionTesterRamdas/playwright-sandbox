5a7fcd0b47fe9e19a8b93ffdfc6cf232a9ac7348
Ramdas <ramdas.qualityfirst@gmail.com>
2026-04-14 18:50:45 +0530

    feat: enhance file upload tests with multiple and buffer file upload scenarios

--- File Upload Practice/fileUpload.spec.ts
+++ File Upload Practice/fileUpload.spec.ts
@@ -1,16 +1,63 @@
-import { expect, test } from "@playwright/test";
+import { expect, Locator, test } from "@playwright/test";
 import path from "path";
 
+const testURL: string =
+  "file:///C:/Projects/playwright-sandbox/File%20Upload%20Practice/demo-upload.html";
+
 test("single file upload", async ({ page }) => {
-  const testURL =
-    "file:///C:/Projects/playwright-sandbox/File%20Upload%20Practice/demo-upload.html";
-  page.goto(testURL);
-  page
+  await page.goto(testURL);
+  await page.waitForTimeout(3000);
+  await page
     .locator("#singleFile")
     .setInputFiles(
       path.join(
-        "C:\Projects\playwright-sandbox\File Upload Practice\Sample files",
-        "file1.txt",
+        "C:/Projects/playwright-sandbox/File Upload Practice/Sample files",
+        "file1.txt.txt",
       ),
     );
+
+  await page.waitForTimeout(2000);
+  await expect(page.getByText("Status: Selected")).toBeVisible();
+});
+
+test("multiple files upload", async ({ page }) => {
+  await page.goto(testURL);
+  await page
+    .locator("#multiFiles")
+    .setInputFiles([
+      path.join(
+        "C:/Projects/playwright-sandbox/File Upload Practice/Sample files",
+        "file1.txt.txt",
+      ),
+      path.join(
+        "C:/Projects/playwright-sandbox/File Upload Practice/Sample files",
+        "file2.txt.txt",
+      ),
+      path.join(
+        "C:/Projects/playwright-sandbox/File Upload Practice/Sample files",
+        "file3.txt.txt",
+      ),
+    ]);
+
+  await expect(page.locator("#multi-status")).toHaveText(
+    "Status: Selected -> file1.txt.txt, file2.txt.txt, file3.txt.txt",
+  );
+
+  await page.waitForTimeout(5000);
+
+  // Remove all selected files
+  await page.locator("#multiFiles").setInputFiles([]);
+
+  await page.waitForTimeout(3000);
+});
+
+test("buffer file upload - file on the go", async ({ page }) => {
+  await page.goto(testURL);
+  await page.getByTestId("buffer-file-input").setInputFiles({
+    name: "file4.txt",
+    mimeType: "text/plain",
+    buffer: Buffer.from("this is a test file 4, created by Ramdas."),
+  });
+
+  await page.waitForTimeout(5000);
 });
